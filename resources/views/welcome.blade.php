bdeb99777e2899ee5f63a266aa5cc5c8cae53531
Jazmin Madel partida castillon <jazminmadelpartidacastillon@MacBook-Pro-de-Jazmin.local>
2019-07-07 12:10:03 +0200

    Added plus-minus input into product grid

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,16 +64,16 @@
 
                         <div class="hover-add-to-cart">
                             <!-- Change the `data-field` of buttons and `name` of input field's for multiple plus minus buttons-->
-                            <div class="input-group plus-minus-input">
+                            <div class="input-group plus-minus-input align-center">
                                 <div class="input-group-button">
-                                    <button type="button" class="button hollow circle" data-quantity="minus" data-field="quantity">
-                                      <i class="fa fa-minus" aria-hidden="true"></i>
+                                    <button type="button" class="button circle" data-quantity="minus" data-field="quantity">
+                                      <i class="fas fa-minus" aria-hidden="true"></i>
                                     </button>
                                 </div>
                                 <input class="input-group-field" type="number" name="quantity" value="0">
                                 <div class="input-group-button">
-                                    <button type="button" class="button hollow circle" data-quantity="plus" data-field="quantity">
-                                      <i class="fa fa-plus" aria-hidden="true"></i>
+                                    <button type="button" class="button circle" data-quantity="plus" data-field="quantity">
+                                      <i class="fas fa-plus" aria-hidden="true"></i>
                                     </button>
                                 </div>
                             </div>
