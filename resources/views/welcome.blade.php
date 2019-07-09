bffbf6d36a5fa5c148e36b11e723dd12d0f05787
Jazmin Madel partida castillon <jazminmadelpartidacastillon@MacBook-Pro-de-Jazmin.local>
2019-07-09 19:37:38 +0200

    Added footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -307,21 +307,51 @@
         </div>
     </div>
 
+    <div class="footer">
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x">
+                <div class="cell medium-6 large-3">
+                    <h4><strong>Get in touch</strong></h4>
+                    <ul class="no-bullet">
+                        <li>FAQs</li>
+                        <li>Give us feedback</li>
+                        <li>Contact us</li>
+                    </ul>
+                </div>
+                <div class="cell medium-6 large-3">
+                    <h4><strong>About World Cinema</strong></h4>
+                    <ul class="no-bullet">
+                        <li>About us</li>
+                        <li>Find us</li>
+                        <li>Schedule</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div class="cell medium-6 large-3">
+                    <h4><strong>Legal stuff</strong></h4>
+                    <ul class="no-bullet">
+                        <li>Terms & Conditions</li>
+                        <li>Privacy policy</li>
+                        <li>Cookie policy</li>
+                    </ul>
+                </div>
+                <div class="cell medium-6 large-3">
+                    <h4><strong>Contact with us</strong></h4>
+                    <ul class="no-bullet">
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Google +</li>
+                    </ul>
+                </div>
+            </div>
 
+            <div class="grid-x grid-padding-x">
+                <div class="cell">
+                    <hr>
+                    © All rights reserved
+                </div>
+            </div>
+        </div>
+    </div>
 
-
-    <div class="grid-container">
-  <div class="grid-x grid-margin-x">
-    <div class="cell small-4">cell</div>
-    <div class="cell small-4">cell</div>
-    <div class="cell small-4">cell</div>
-  </div>
-</div>
-
-
-<div class="slider" data-slider data-initial-start="50" data-end="200">
-  <span class="slider-handle"  data-slider-handle role="slider" tabindex="1"></span>
-  <span class="slider-fill" data-slider-fill></span>
-  <input type="hidden">
-</div>
 @endsection
